f14eaa7eebf52cf646a33c28d3200b9983d6960e
Shashikant Burnwal <shashikant.burnwal@gmail.com>
2021-05-25 14:07:35 +0530

    share code on github

---
+++ src/data.js
@@ -0,0 +1,80 @@
+const data={
+    products:[
+        {
+            _id:'1',
+            name:'Nike Slim Shirt',
+            category:'Shirts',
+            image:'/images/p1.jpeg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+
+        },
+
+        {
+            _id:'2',
+            name:'Adidas Fit Shirt',
+            category:'Shirts',
+            image:'/images/p2.jpeg',
+            price: 100,
+            brand: 'Adidas',
+            rating: 4.0,
+            numReviews: 10,
+            description: 'high quality product'
+
+        },
+        {
+            _id:'3',
+            name:'Lacoste Free Shirt',
+            category:'Shirts',
+            image:'/images/p3.jpeg',
+            price: 130,
+            brand: 'Lacoste',
+            rating: 5,
+            numReviews: 10,
+            description: 'high quality product'
+
+        },
+        {
+            _id:'4',
+            name:'Nike Slim Pant',
+            category:'Pants',
+            image:'/images/p4.jpeg',
+            price: 110,
+            brand: 'Nike',
+            rating: 4.0,
+            numReviews: 10,
+            description: 'high quality product'
+
+        },
+        {
+            _id:'5',
+            name:'Puma Slim Shirt',
+            category:'Pants',
+            image:'/images/p5.jpeg',
+            price: 65,
+            brand: 'Nike',
+            rating: 3.5,
+            numReviews: 10,
+            description: 'high quality product'
+
+        },
+        {
+            _id:'6',
+            name:'Adidas Fit Pant',
+            category:'Pants',
+            image:'/images/p6.jpeg',
+            price: 95,
+            brand: 'Adidas',
+            rating: 4.0,
+            numReviews: 10,
+            description: 'high quality product'
+
+        }
+    ],
+};
+
+export default data;
+
