31166a0c8feef574478f3c16102ba9afdd276cbb
Shashikant Burnwal <shashikant.burnwal@gmail.com>
2021-05-27 14:40:59 +0530

    redux added for productscreen

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,18 @@ app.get('/', (req,res)=>{
     res.send('Server is ready');
 });
 
-app.get ('/api/products', (req,res)=>{
+app.get ('/api/products', (req, res)=>{
     res.send(data.products);
+
+});
+
+app.get ('/api/products/:id', (req, res)=>{
+    const product = data.products.find ( (product) => product._id===req.params.id);
+    if (product){
+        res.send (product);
+    } else{
+        res.status(404).send ({message: 'Product not found' });
+    }
 
 });
 
